436a233d3c1d97b8012c48988e20d84a81e3e6e0
Tom <tom@skuse.me>
2024-11-19 17:57:22 +1100

    set vite base path

--- vite.config.mjs
+++ vite.config.mjs
@@ -14,6 +14,7 @@ import { fileURLToPath, URL } from "node:url"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "aprs-smart-beacon-calculator",
     build: {
         outDir: "./dist"
     },
